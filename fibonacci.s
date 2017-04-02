dae1474ce3d137106c9d35ae16362c87d795b3ae
SergioGausaker <sergio@gausaker.com>
2017-04-02 17:48:33 +0200

    Eliminar push y pop

--- fibonacci.s
+++ fibonacci.s
@@ -1,9 +1,5 @@
-.text
-
 fib:
-	mov r0, #8
-	sub sp, sp, #4
-	str r4,[sp]	
+	mov r0, #8	
 	mov r1, #0
 	mov r2, #1
 	cmp r0, #0
@@ -20,6 +16,4 @@ loop:
 	b loop
 sal:
 	mov r0, r2
-	ldr r4,[sp]
-	add sp, sp, #4
 	mov pc, lr
